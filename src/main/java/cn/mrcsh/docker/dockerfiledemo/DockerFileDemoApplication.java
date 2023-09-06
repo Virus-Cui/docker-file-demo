d09526ed38143596d32b2919b480be1dfe9b9830
起飞 <Virus_Cui@outlook.com>
2023-09-06 14:58:23 +0800

    initialize

---
+++ src/main/java/cn/mrcsh/docker/dockerfiledemo/DockerFileDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.mrcsh.docker.dockerfiledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerFileDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerFileDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/mrcsh/docker/dockerfiledemo/DockerFileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mrcsh.docker.dockerfiledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerFileDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
